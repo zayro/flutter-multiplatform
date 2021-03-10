128dcb0d159deec6a5261ed21a462a023f30b6bf
marlon zayro <zayro8905@gmail.com>
2021-03-10 10:33:13 -0500

    buscador original

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,112 +1,227 @@
 import 'package:flutter/material.dart';
-import './details.dart';
 
 class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  TabController controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller = new TabController(length: 4, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF21BFBD),
-      body: Column(
+      body: ListView(
         children: <Widget>[
-          Container(
-              //height: MediaQuery.of(context).size.height - 185.0,
-              height: MediaQuery.of(context).size.height * 0.2,
-              child: Padding(
-                padding: EdgeInsets.only(left: 40.0),
-                child: Row(
-                  children: <Widget>[
-                    Text('Healthy',
-                        style: TextStyle(
-                            fontFamily: 'Montserrat',
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 25.0)),
-                    SizedBox(width: 10.0),
-                    Text('Food',
-                        style: TextStyle(
-                            fontFamily: 'Montserrat',
-                            color: Colors.white,
-                            fontSize: 25.0))
-                  ],
-                ),
-              )),
-          Container(
-            //height: MediaQuery.of(context).size.height - 185.0,
-            height: MediaQuery.of(context).size.height * 0.8,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(50.0),
-                  topRight: Radius.circular(50.0)),
-            ),
-            child: ListView(
-              padding: EdgeInsets.only(left: 25.0, right: 20.0),
-              children: <Widget>[
-                Container(
-                    //height: MediaQuery.of(context).size.height * 0.5,
-                    child: ListView.builder(
-                        shrinkWrap: true,
-                        physics: NeverScrollableScrollPhysics(),
-                        itemCount: 10,
-                        itemBuilder: (context, index) {
-                          return _buildFoodItem(
-                              'assets/plate1.png', 'Salmon bowl', '\$24.00');
-                        })),
-              ],
-            ),
+          Column(
+            children: <Widget>[
+              Stack(
+                children: <Widget>[
+                  Container(
+                    height: 250.0,
+                    width: double.infinity,
+                    color: Colors.yellow[100],
+                  ),
+                  Positioned(
+                    bottom: 50.0,
+                    right: 100.0,
+                    child: Container(
+                      height: 400.0,
+                      width: 400.0,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(200.0),
+                          color: Colors.yellow[200].withOpacity(0.4)),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 100.0,
+                    left: 150.0,
+                    child: Container(
+                        height: 300.0,
+                        width: 300.0,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(150.0),
+                            color: Colors.yellow[300].withOpacity(0.5))),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      SizedBox(height: 15.0),
+                      Row(
+                        children: <Widget>[
+                          SizedBox(width: 15.0),
+                          SizedBox(
+                              width: MediaQuery.of(context).size.width - 120.0),
+                          SizedBox(height: 15.0),
+                        ],
+                      ),
+                      SizedBox(height: 50.0),
+                      Padding(
+                        padding: EdgeInsets.only(left: 15.0),
+                        child: Text(
+                          'Hello , Pino',
+                          style: TextStyle(
+                              fontFamily: 'Quicksand',
+                              fontSize: 30.0,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      SizedBox(height: 15.0),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 15.0),
+                        child: Text(
+                          'What do you want to buy?',
+                          style: TextStyle(
+                              fontFamily: 'Quicksand',
+                              fontSize: 23.0,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      SizedBox(height: 25.0),
+                      Padding(
+                        padding: EdgeInsets.only(left: 15.0, right: 15.0),
+                        child: Material(
+                          elevation: 5.0,
+                          borderRadius: BorderRadius.circular(5.0),
+                          child: TextField(
+                              onTap: () {
+                                Navigator.of(context).pushNamed('/search');
+                              },
+                              decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  prefixIcon: Icon(Icons.search,
+                                      color: Colors.yellow[400], size: 30.0),
+                                  contentPadding:
+                                      EdgeInsets.only(left: 15.0, top: 15.0),
+                                  hintText: 'Search',
+                                  hintStyle: TextStyle(
+                                      color: Colors.grey,
+                                      fontFamily: 'Quicksand'))),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              Stack(
+                children: <Widget>[
+                  SizedBox(height: 10.0),
+                  Material(
+                      elevation: 1.0,
+                      child: Container(height: 75.0, color: Colors.white)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Container(
+                        height: 75.0,
+                        width: MediaQuery.of(context).size.width / 4,
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              height: 50.0,
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: AssetImage('assets/sofas.png'))),
+                            ),
+                            Text(
+                              'Sofas',
+                              style: TextStyle(fontFamily: 'Quicksand'),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 75.0,
+                        width: MediaQuery.of(context).size.width / 4,
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              height: 50.0,
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image:
+                                          AssetImage('assets/wardrobe.png'))),
+                            ),
+                            Text(
+                              'Wardrobe',
+                              style: TextStyle(fontFamily: 'Quicksand'),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 75.0,
+                        width: MediaQuery.of(context).size.width / 4,
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              height: 50.0,
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: AssetImage('assets/desks.png'))),
+                            ),
+                            Text(
+                              'Desk',
+                              style: TextStyle(fontFamily: 'Quicksand'),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 75.0,
+                        width: MediaQuery.of(context).size.width / 4,
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              height: 50.0,
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image:
+                                          AssetImage('assets/dressers.png'))),
+                            ),
+                            Text(
+                              'Dresser',
+                              style: TextStyle(fontFamily: 'Quicksand'),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ],
           )
         ],
       ),
+      bottomNavigationBar: Material(
+        color: Colors.white,
+        child: TabBar(
+          controller: controller,
+          indicatorColor: Colors.yellow,
+          tabs: <Widget>[
+            Tab(icon: Icon(Icons.event_seat, color: Colors.yellow)),
+            Tab(icon: Icon(Icons.timer, color: Colors.grey)),
+            Tab(icon: Icon(Icons.shopping_cart, color: Colors.grey)),
+            Tab(icon: Icon(Icons.person_outline, color: Colors.grey))
+          ],
+        ),
+      ),
     );
-  }
-
-  Widget _buildFoodItem(String imgPath, String foodName, String price) {
-    return Padding(
-        padding: EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0),
-        child: InkWell(
-            onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => DetailsPage(
-                      heroTag: imgPath, foodName: foodName, foodPrice: price)));
-            },
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Container(
-                    child: Row(children: [
-                  Hero(
-                      tag: imgPath,
-                      child: Image(
-                          image: AssetImage(imgPath),
-                          fit: BoxFit.cover,
-                          height: 75.0,
-                          width: 75.0)),
-                  SizedBox(width: 10.0),
-                  Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(foodName,
-                            style: TextStyle(
-                                fontFamily: 'Montserrat',
-                                fontSize: 17.0,
-                                fontWeight: FontWeight.bold)),
-                        Text(price,
-                            style: TextStyle(
-                                fontFamily: 'Montserrat',
-                                fontSize: 15.0,
-                                color: Colors.grey))
-                      ])
-                ])),
-                IconButton(
-                    icon: Icon(Icons.add),
-                    color: Colors.black,
-                    onPressed: () {})
-              ],
-            )));
   }
 }
--- lib/routes/index.dart
+++ lib/routes/index.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 import '../pages/details/detalle.widget.dart';
 import '../pages/home/home.widget.dart';
 import '../pages/home/home.dart';
-import '../pages/home/details.dart';
+import '../pages/products/details.dart';
 import '../pages/login/login.dart';
 import '../pages/login/signup.dart';
+import '../pages/search/search.dart';
 
 Map<String, WidgetBuilder> rootPath() {
   return <String, WidgetBuilder>{
@@ -13,6 +14,7 @@ Map<String, WidgetBuilder> rootPath() {
     "/detail": (BuildContext context) => DetailProductPage(),
     "/details": (BuildContext context) => DetailsPage(),
     "/login": (BuildContext context) => LoginPage(),
-    '/signup': (BuildContext context) => SignupPage()
+    '/signup': (BuildContext context) => SignupPage(),
+    '/search': (BuildContext context) => SearchPage()
   };
 }
